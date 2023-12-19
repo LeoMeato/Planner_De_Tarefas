d4fefde3c8f6db7e1645aca87681d598f599dbd5
HamsterGulloso <danielgullovasconcellos@gmail.com>
2023-12-19 05:31:26 -0300

    Com sqlite em desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:Planner_De_Tarefas/helper.dart';
 import 'package:Planner_De_Tarefas/login.dart';
 import 'package:Planner_De_Tarefas/model.dart';
@@ -8,8 +10,14 @@ import 'package:Planner_De_Tarefas/tarefas_recentes.dart';
 import 'package:Planner_De_Tarefas/testes.dart';
 import 'package:flutter/material.dart';
 import 'package:Planner_De_Tarefas/home.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
+  if(Platform.isLinux || Platform.isWindows){
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(MaterialApp(
     title: 'Planner de Tarefas',
     initialRoute: "/",
